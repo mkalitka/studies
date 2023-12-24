ac5f81e7f6427fc2a66979aeba80b82565951045
mkalitka <mikolaj.kalitka@proton.me>
2023-12-24 02:47:00 +0100

    KONKURS

--- an/lista8/zad7.py
+++ an/lista8/zad7.py
@@ -1,15 +1,24 @@
-from data import xs as xs_data, ys as ys_data
 from nifs3 import get_s
+import data
 import matplotlib.pyplot as plt
 
-ts_data = [k / len(xs_data) for k in range(len(xs_data))]
-sx = get_s(ts_data, xs_data)
-sy = get_s(ts_data, ys_data)
+sx1 = get_s(data.ts1, data.xs1)
+sy1 = get_s(data.ts1, data.ys1)
+plt.plot([sx1(u) for u in data.us1], [sy1(u) for u in data.us1])
 
-M = 1000
-us = [k / M for k in range(M)]
+sx2 = get_s(data.ts2, data.xs2)
+sy2 = get_s(data.ts2, data.ys2)
+plt.plot([sx2(u) for u in data.us2], [sy2(u) for u in data.us2])
 
-plt.plot(xs_data, ys_data)
-plt.plot([sx(u) for u in us], [sy(u) for u in us])
+sx3 = get_s(data.ts3, data.xs3)
+sy3 = get_s(data.ts3, data.ys3)
+plt.plot([sx3(u) for u in data.us3], [sy3(u) for u in data.us3])
+
+sx4 = get_s(data.ts4, data.xs4)
+sy4 = get_s(data.ts4, data.ys4)
+plt.plot([sx4(u) for u in data.us4], [sy4(u) for u in data.us4])
+
+plt.xlim(0.0, 180.0)
+plt.ylim(-4, 20.0)
 plt.show()
 
